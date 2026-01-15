0f550c6fdade72289dff29986a04669a5e60d520
Searidang Pa <dangsyncpa@gmail.com>
2026-01-15 13:49:06 -0500

    Inline provider validation error `vim.notify` call

--- lua/blackboard/init.lua
+++ lua/blackboard/init.lua
@@ -4,10 +4,6 @@ local actions = require 'blackboard.bookmarks.actions'
 
 local M = {}
 
----@param msg string
-local function notify_err(msg)
-  vim.notify(msg, vim.log.levels.ERROR)
-end
 
 ---@return blackboard.MarkProvider
 local function get_provider()
@@ -24,13 +20,16 @@ M.setup = function(opts)
 
   local provider = get_provider()
   if
-    type(provider) ~= 'table'
-    or type(provider.list_marks) ~= 'function'
-    or type(provider.set_mark) ~= 'function'
-    or type(provider.unset_mark) ~= 'function'
-    or type(provider.jump_to_mark) ~= 'function'
+      type(provider) ~= 'table'
+      or type(provider.list_marks) ~= 'function'
+      or type(provider.set_mark) ~= 'function'
+      or type(provider.unset_mark) ~= 'function'
+      or type(provider.jump_to_mark) ~= 'function'
   then
-    notify_err 'blackboard: mark_provider must implement list_marks/set_mark/unset_mark/jump_to_mark'
+    vim.notify(
+      'blackboard: mark_provider must implement list_marks/set_mark/unset_mark/jump_to_mark',
+      vim.log.levels.ERROR
+    )
   end
 
   -- Setup keybindings
